91f5c083547e8db38b81670b9405de28bed100ae
Loryan Strant <loryan@strant.com>
2026-05-31 21:03:47 +1000

    Import from external tool: rollup.config.mjs

---
+++ rollup.config.mjs
@@ -0,0 +1,23 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import json from "@rollup/plugin-json";
+import terser from "@rollup/plugin-terser";
+
+const dev = process.env.ROLLUP_WATCH === "true";
+
+export default {
+  input: "src/home-connect-oven-card.ts",
+  output: {
+    file: "dist/home-connect-oven-card.js",
+    format: "es",
+    sourcemap: dev,
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({ tsconfig: "./tsconfig.json" }),
+    json(),
+    !dev && terser({ format: { comments: false } }),
+  ],
+};
